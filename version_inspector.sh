40bf8a6d44c3287f72659789cb6bd27d3a3ae901
Yaniv Setton <yanivsiton89@gmail.com>
2019-06-29 13:23:42 +0300

    Upload SDK

---
+++ version_inspector.sh
@@ -0,0 +1,1 @@
+cat /etc/*release | grep VERSION= && java -version && gradle -v && kotlinc -version && cat $ANDROID_HOME/tools/source.properties | grep Pkg.Revision && sdkmanager --list && apt list --installed | grep openssh-server
